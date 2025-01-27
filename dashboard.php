73375d1cd134259051a0038750f3481efcd3de56
Rohan25096 <120667378+Rohan25096@users.noreply.github.com>
2025-01-27 14:52:07 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "Pk3745st@";
-$dbname = "office";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
